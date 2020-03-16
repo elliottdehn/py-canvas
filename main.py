79ae373f0e2c1f90b819b78e18066f606a18d4df
elliottdehnbostel <dehnbostele@gmail.com>
2020-03-16 17:38:42 -0500

    basic socket stuff

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
-def index():
+def renderCanvas():
     return render_template('canvas.html')
+
+@socketio.on('draw')
+def handle_draw_event(event):
+    print('received event: ' + event)
+
+if __name__ == '__main__':
+    socketio.run(app)
