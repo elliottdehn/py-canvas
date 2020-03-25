53f65906cf02c8a1d193f50fc3eac237072765ae
elliottdehnbostel <dehnbostele@gmail.com>
2020-03-24 21:15:58 -0500

    persistence exists now

--- experiments.py
+++ experiments.py
@@ -4,8 +4,8 @@ import os
 
 class Event:
     bytesPerEvent = 9
-    def __init__(self, bytes):
-        self.bytes = bytes
+    def __init__(self, bs: bytes):
+        self.bs = bs
 
 class EventCR:
     def addEvent(self, e: Event) -> Event:
@@ -17,21 +17,21 @@ class EventCR:
         pass
 
 class SimpleDB(EventCR):
+
+    def __init__(self):
+        self.db = open("events.db", "ab+")
     
     def addEvent(self, e: Event) -> Event:
-        f = open("events.db", "ab+")
-        print(e.bytes)
-        f.write(e.bytes)
-        f.close()
+        self.db.write(e.bs)
+        self.db.flush()
+        os.fsync(self.db.fileno())
         return e
     
     def getEvents(self, startId: int, count: int) -> List[Event]:
         if count < 1:
             return None
-        f = open("events.db", "rb+")
-        mm = mmap(f.fileno(), length=count * Event.bytesPerEvent, offset=startId * Event.bytesPerEvent)
-        f.close()
+        mm = mmap(self.db.fileno(), length=count * Event.bytesPerEvent, offset=startId * Event.bytesPerEvent)
         return list(map(lambda bs: Event(bs), group_per(mm, Event.bytesPerEvent)))
 
 def group_per(source, step):
-    return [source[sliceStart:sliceStart+step] for sliceStart in range(0, len(source), step)]
+    return [source[sliceStart: sliceStart + step] for sliceStart in range(0, len(source), step)]
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, join_room
 from sys import getsizeof
+from experiments import SimpleDB, Event
+from queue import Queue
+from threading import Thread
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+db = SimpleDB()
+
+write_buffer = Queue(-1)
+class Consumer(Thread):
+    def __init__(self, q: Queue, f):
+        super(Consumer, self).__init__()
+        self.buffer = q
+        self.f = f
+
+    def run(self):
+        for elm in iter(self.buffer.get, None):
+            self.f(elm)
+
+Consumer(write_buffer, lambda line: db.addEvent(Event(line))).start()
 
 @app.route('/')
 def renderCanvas():
@@ -12,6 +29,7 @@ def renderCanvas():
 
 @socketio.on('d')
 def handle_draw(line):
+    write_buffer.put(line)
     socketio.emit('r', line, broadcast=True)
 
 if __name__ == '__main__':
