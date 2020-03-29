30553e11cfbd4f2eec11776f15b8114d81da3250
elliottdehnbostel <dehnbostele@gmail.com>
2020-03-28 23:38:43 -0500

    added canvas state and tests for it

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from sys import getsizeof
 from persistence.event.events import SimpleDBv2, Event
 from queue import Queue
 from threading import Thread
+from canvas import Canvas, Pixel
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 db = SimpleDBv2("events.db")
+canvas = Canvas(width=1600, height=800)
 
 write_buffer = Queue(-1)
 class Consumer(Thread):
@@ -30,6 +32,10 @@ def renderCanvas():
 @socketio.on('d')
 def handle_draw(line):
     write_buffer.put(line)
+    line_e = Event(line)
+    start_pixel = Pixel(line_e.get_sx(), line_e.get_sy(), line_e.get_c_flag())
+    end_pixel = Pixel(line_e.get_ex(), line_e.get_ey(), line_e.get_c_flag())
+    canvas.set_line(start_pixel, end_pixel)
     socketio.emit('r', line, broadcast=True)
 
 if __name__ == '__main__':
